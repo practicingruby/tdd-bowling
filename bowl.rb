b4b051ee049e34136e730a63e962d6ad0038c8e7
Chenoa <cowmanifestation@gmail.com>
2009-07-22 10:04:43 -0400

    test must add throws

--- bowl.rb
+++ bowl.rb
@@ -1,8 +1,9 @@
 class Bowler
 
   def throw(ball)
-
+    @score = ball
   end
+  
 
   attr_reader :score
 
@@ -21,7 +22,11 @@ class BowlerTest < Test::Unit::TestCase
     @bowler.throw(3)
     assert_equal 3, @bowler.score
   end
+  
+  def test_must_add_throws
+    @bowler.throw(3)
+    @bowler.throw(4)
+    assert_equal 7, @bowler.score
+  end
 
 end
-
-
