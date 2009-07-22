b914ba0dc06900c0f58106d870030feb300c0945
Gregory Brown <gregory.t.brown@gmail.com>
2009-07-22 10:11:17 -0400

    Bowler should not return a score when score isn't determinable

--- bowl.rb
+++ bowl.rb
@@ -1,12 +1,15 @@
 class Bowler
+  ScoreUnknown = Class.new(StandardError)
+
+  def initialize
+    @score = 0
+  end
 
   def throw(ball)
-    @score = ball
+    @score += ball
   end
-  
 
   attr_reader :score
-
 
 end
 
@@ -27,6 +30,16 @@ class BowlerTest < Test::Unit::TestCase
     @bowler.throw(3)
     @bowler.throw(4)
     assert_equal 7, @bowler.score
+  end
+
+  def test_must_not_provide_a_score_when_score_is_pending
+    @bowler.throw(5)
+    @bowler.throw(5)
+    assert_raises(Bowler::ScoreUnknown) do
+      @bowler.score
+    end
+    
+    assert_equal @bowler.state, :spare
   end
 
 end
